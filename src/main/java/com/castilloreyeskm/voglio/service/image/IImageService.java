0137aab854322d83cdd96c8332acd09515fb06d8
Kevin Castillo <castilloreyeskm@gmail.com>
2024-09-03 12:18:16 -0500

    Create ImageService

---
+++ src/main/java/com/castilloreyeskm/voglio/service/image/IImageService.java
@@ -0,0 +1,18 @@
+package com.castilloreyeskm.voglio.service.image;
+
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.castilloreyeskm.voglio.dto.ImageDto;
+import com.castilloreyeskm.voglio.model.Image;
+
+public interface IImageService {
+	Image getImageById(Long id);
+
+	void deleteImageById(Long id);
+
+	List<ImageDto> saveImages(Long productId, List<MultipartFile> files);
+
+	void updateImage(MultipartFile file, Long imageId);
+}
